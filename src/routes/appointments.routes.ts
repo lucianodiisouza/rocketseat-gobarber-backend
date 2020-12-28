840b3ee4755d4fb7a596a8478d7dc046c5af87fb
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-12-28 10:36:18 -0300

    re-starting gobarber backend

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,6 +5,12 @@ import { AppointmentsRepository } from '../repositories';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all;
+
+  return res.json(appointments);
+});
+
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
   const parsedDate = startOfHour(parseISO(date));
