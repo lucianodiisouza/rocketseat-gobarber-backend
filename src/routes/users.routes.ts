062bb4bcc28d75aa63589b35fcad04b2d2d75f00
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-10 16:15:46 -0300

    done: hash users passoword at creating time

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -16,7 +16,15 @@ usersRouter.post('/', async (req, res) => {
       password,
     });
 
-    return res.json(user);
+    const userData = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return res.json(userData);
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
