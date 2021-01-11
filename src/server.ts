1e09b596236b7d1104e1418f2736e687d0c97a9e
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-10 22:21:07 -0300

    done: serve static place to preview images

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
-import routes from './routes';
+
 import './database';
+
+import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.get('/', (req, res) => {
   return res.json({ serverStatus: 'running...' });
